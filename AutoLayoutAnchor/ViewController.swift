616b2f93d5002d47614f89bf5597eb824bce6f45
kevinbai0 <kevin.c.bai0@gmail.com>
2018-01-06 01:58:06 -0500

    Added in AutoLayoutAnchor File and updated ViewController with example views

--- AutoLayoutAnchor/ViewController.swift
+++ AutoLayoutAnchor/ViewController.swift
@@ -1,8 +1,8 @@
 //
 //  ViewController.swift
-//  AutoLayoutAnchor
+//  Lib
 //
-//  Created by Kevin Bai on 2018-01-06.
+//  Created by Kevin Bai on 2018-01-05.
 //  Copyright © 2018 Kevin Bai. All rights reserved.
 //
 
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let rectangleView = UIView()
+        rectangleView.backgroundColor = .orange
+        self.view.addSubview(rectangleView)
+        rectangleView.setConstraints(to: self.view, .left([5]), .right([-5, Anchor.centerX]), .top([Anchor.centerY]), .height([300]))
+        
+        let view2 = UIView()
+        view2.backgroundColor = .red
+        view2.addToView(self.view, .left([Anchor.right, rectangleView, 10]), .right([-10]), .top([rectangleView]), .height([rectangleView]))
     }
 
     override func didReceiveMemoryWarning() {
